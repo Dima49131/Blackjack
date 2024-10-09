4e0924398771b86658e253bb5b5ff4e40a2f2068
Dima49131 <dima.8.anderson@gmail.com>
2024-10-09 17:51:51 -0500

    Keyframes css feature, updated pages, banner fixes

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -27,4 +27,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Uncomment to enable automatic scrolling
     autoScroll();
+
+
+    window.addEventListener('scroll', function() {
+        var scrollPosition = window.scrollY; // Current scroll position
+        var textContainers = document.querySelectorAll('.gameTile'); // Select specific elements to fade in/out
+        textContainers.forEach(function(textContainer) {
+            var containerOffset = textContainer.getBoundingClientRect().top + scrollPosition; // Top offset relative to the document
+            var distanceFromTop = containerOffset - scrollPosition; // Distance from the current scroll position
+
+            // Adjust this value based on when you want the fade effect to trigger
+            if (distanceFromTop < window.innerHeight * 0.2) { // Fades in when 60% of the viewport is reached
+                textContainer.classList.add('fade-in');
+                textContainer.classList.remove('fade-out');
+                console.log("fade out");
+                
+            } else {
+                textContainer.classList.remove('fade-in');
+                textContainer.classList.add('fade-out');
+            }
+        });
+    });
+
+
 });
